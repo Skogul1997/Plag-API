847dc879e0f1b880ae92033ead1b5b4c3cfdefa9
Skogul1997 <kogul.17@cse.mrt.ac.lk>
2020-06-10 22:10:08 +0530

    Added Server invalid response

--- moss.py
+++ moss.py
@@ -3,6 +3,7 @@ import os
 import webscraper
 import multiprocessing
 import time
+import telnetlib
 # https://group13-plagiarism-api.herokuapp.com/
 # MOSS userID = 669827647
 userID = int(os.environ.get("MOSS_USER_ID"))
@@ -27,6 +28,10 @@ def getURL(path, lang):
     directory = os.path.join(os.getcwd(), path)
     os.chdir(path)
 
+    try:
+        tn = telnetlib.Telnet('moss.stanford.edu', 7690)
+    except:
+        return {'status': "Fail", 'error': "MOSS server not available."}, 503
     # for file in os.listdir(directory):
     #     filename = os.fsdecode(file)
     #     m.addFile(filename)
@@ -36,6 +41,7 @@ def getURL(path, lang):
         target=findUrl, args=(userID, lang, q, directory, path, a))
 
     p.start()
+
     # start_time = time.time()
     while(True):
         if(p.is_alive()):
@@ -43,14 +49,14 @@ def getURL(path, lang):
         else:
             break
     # time.sleep(150)
-    try:
-        a = q.get()
-        url = q.get()
-        b = webscraper.getResponse(url)
-        a.update(b)
-        return {'status': 'Success', 'results': a, 'detailed_report_url': url}, 200
-    except:
-        return {'status': "Fail", 'error': "MOSS server not available."}, 503
+    # try:
+    a = q.get()
+    url = q.get()
+    b = webscraper.getResponse(url)
+    a.update(b)
+    return {'status': 'Success', 'results': a, 'detailed_report_url': url}, 200
+    # except:
+    #     return {'status': "Fail", 'error': "MOSS server not available."}, 503
 
     # if (p.is_alive() == False):
     #     a = q.get()
