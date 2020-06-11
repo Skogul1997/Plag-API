e469f3601444c529a46a7395d9a41274525bc7e6
Skogul1997 <kogul.17@cse.mrt.ac.lk>
2020-06-11 13:44:16 +0530

    Added detection of server malfunction.

--- moss.py
+++ moss.py
@@ -4,6 +4,7 @@ import webscraper
 import multiprocessing
 import time
 import telnetlib
+from datetime import datetime
 # https://group13-plagiarism-api.herokuapp.com/
 # MOSS userID = 669827647
 userID = int(os.environ.get("MOSS_USER_ID"))
@@ -32,6 +33,7 @@ def getURL(path, lang):
         tn = telnetlib.Telnet('moss.stanford.edu', 7690)
     except:
         return {'status': "Fail", 'error': "MOSS server not available."}, 503
+
     # for file in os.listdir(directory):
     #     filename = os.fsdecode(file)
     #     m.addFile(filename)
@@ -39,12 +41,17 @@ def getURL(path, lang):
 
     p = multiprocessing.Process(
         target=findUrl, args=(userID, lang, q, directory, path, a))
-
+    start_time = datetime.now()
     p.start()
 
     # start_time = time.time()
     while(True):
         if(p.is_alive()):
+            if((datetime.now()-start_time).total_seconds()>=180):
+                p.terminate()
+                p.join()
+                return {'status': "Fail", 'error': "Server taking too long to process."}, 503
+                break
             continue
         else:
             break
